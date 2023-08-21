4df41324b7bab28ae22a4ed1ee8ff9cbb337e058
harshita <harshitakatara34>
2023-08-21 17:06:14 +0530

    backend

--- index.js
+++ index.js
@@ -8,22 +8,20 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routers
 app.use("/", route);
 app.use("/", appointments);
 
-const PORT = process.env.PORT; // Use the PORT environment variable or default to 9090
+const PORT = process.env.PORT;
 
 app.listen(PORT, async () => {
   try {
     await connection;
     console.log("Connected to DB");
   } catch (error) {
-    console.error("Error connecting to DB:", error);
+    console.error(error);
   }
   console.log(`Server is running on port ${PORT}`);
 });
